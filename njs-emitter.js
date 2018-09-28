cd2eca8d15f53a596a348eb20948b84a52d992a8
madhu10kr <madhukiran319@gmail.com>
2018-09-28 14:54:11 +0530

    object decryption

---
+++ njs-emitter.js
@@ -0,0 +1,51 @@
+const WebSocket = require('ws');
+ 
+const ws = new WebSocket('ws://localhost:8080');
+
+const aesjs = require('aes-js');
+
+const sha256 = require('sha256');
+
+let data = [{name: "kiran",origin:"banglore",destination:"kurnool"},{name: "ravi",origin:"mumbai",destination:"banglore"},{name: "srinu",origin:"delhi",destination:"kolkata"},{name: "ramesh",origin:"banglore",destination:"mumbai"},{name: "balu",origin:"delhi",destination:"kurnool"}];
+
+
+
+let array = [];
+let time;
+let passKey = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+
+
+data.forEach(ele => {
+
+    
+    ele.secret_key = sha256(JSON.stringify(ele));
+
+var textBytes = aesjs.utils.utf8.toBytes(JSON.stringify(ele));
+
+var aesCtr = new aesjs.ModeOfOperation.ctr(passKey, new aesjs.Counter(1));
+var encryptedBytes = aesCtr.encrypt(textBytes);
+
+var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+array.push(encryptedHex)
+});
+
+
+
+ws.on('open', function open() {
+  time = setInterval(function() {
+    let random = array[Math.floor(Math.random() * array.length)];
+    ws.send(`${random}|`);
+},1000);
+});
+
+setTimeout(function() {
+    clearInterval(time);
+    ws.close();
+},10000);
+ 
+
+
+ws.on('message', function incoming(data) {
+  console.log(data);
+});
---
+++ njs-receiver.js
@@ -0,0 +1,46 @@
+const WebSocket = require('ws');
+
+const aesjs = require('aes-js');
+
+const sha256 = require('sha256');
+ 
+const wss = new WebSocket.Server({ port: 8080 });
+
+let receivedData = '';
+let array = [];
+let decryptedValues = [];
+let passKey = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+  // console.log('received: %s', message);
+    receivedData += message;
+  });
+
+  ws.on('close',function() {
+    array = receivedData.split('|');
+    array.pop();
+    array.forEach(value => {
+      decryptedValues.push(decryption(value));
+    });
+    console.log(decryptedValues);
+  })
+ 
+ // ws.send('something');
+});
+
+
+function decryption(value) {
+
+  var encryptedBytes = aesjs.utils.hex.toBytes(value);
+
+  var aesCtr = new aesjs.ModeOfOperation.ctr(passKey, new aesjs.Counter(1));
+  var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+  //console.log(decryptedBytes)
+
+  var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+  //console.log((decryptedText));
+
+  return decryptedText;
+}
